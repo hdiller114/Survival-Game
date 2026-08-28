44dd661e29991e4575685ac90962f27058927ae5
hdiller114 <harrisdillerdesign@gmail.com>
2026-08-28 03:09:58 -0400

    Delete Survival_Game_b0.9 directory

--- Survival_Game_b0.9/Compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-javac src\*.java -d bin
-jar cfm Survival_Game_b0.9.jar MANIFEST.MF -C bin . textures
-pause
--- Survival_Game_b0.9/Run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd C:\Users\Harri\Desktop\Projects\Survival Game\Survival_Game_b0.9
-java -jar Survival_Game_b0.9.jar
-pause
